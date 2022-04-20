d78b697d774a012ff64daae77d18d2df191c329d
Jean Francois Munyaneza <mjeanfrancois360@gmail.com>
2022-04-20 23:32:04 +0200

    Add NeedHelp Component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,85 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+{/* Footer */}
+  <div id="footer"> 
+    <div className="utf-footer-section-item-block">
+      <div className="container">
+        <div className="row">
+		  <div className="col-xl-4 col-md-12">
+			<div className="utf-footer-item-links">
+				<a href="index-1.html"><img className="footer-logo" src="assets/images/footer_logo.png" alt="" /></a>
+				<p>Lorem Ipsum is simply dummy text of printing and type setting industry. Lorem Ipsum been industry standard dummy text ever since, when unknown printer took a galley type scrambled.</p>								
+			</div>
+          </div>
+		  
+          <div className="col-xl-2 col-md-3 col-sm-6">
+            <div className="utf-footer-item-links">
+              <h3>Job Categories</h3>
+              <ul>
+                <li><a href="jobs-list-layout-leftside.html"><i className="icon-feather-chevron-right"></i> <span>Developement</span></a></li>
+                <li><a href="jobs-list-layout-leftside.html"><i className="icon-feather-chevron-right"></i> <span>Designing</span></a></li>
+                <li><a href="jobs-list-layout-leftside.html"><i className="icon-feather-chevron-right"></i> <span>Marketing</span></a></li>
+                <li><a href="jobs-list-layout-leftside.html"><i className="icon-feather-chevron-right"></i> <span>Data Analytics</span></a></li>				
+				<li><a href="/"><i className="icon-feather-chevron-right"></i> <span>Post New Job</span></a></li>				
+              </ul>
+            </div>
+          </div>
+          
+          <div className="col-xl-2 col-md-3 col-sm-6">
+            <div className="utf-footer-item-links">
+              <h3>Job Type</h3>
+              <ul>
+                <li><a href="jobs-list-layout-leftside.html"><i className="icon-feather-chevron-right"></i> <span>Work from Home</span></a></li>
+                <li><a href="jobs-list-layout-leftside.html"><i className="icon-feather-chevron-right"></i> <span>Internship Job</span></a></li>
+				<li><a href="jobs-list-layout-leftside.html"><i className="icon-feather-chevron-right"></i> <span>Freelancer Job</span></a></li>
+                <li><a href="jobs-list-layout-leftside.html"><i className="icon-feather-chevron-right"></i> <span>Part Time Job</span></a></li>
+				<li><a href="jobs-list-layout-leftside.html"><i className="icon-feather-chevron-right"></i> <span>Full Time Job</span></a></li>					
+              </ul>
+            </div>
+          </div>
+          
+          <div className="col-xl-2 col-md-3 col-sm-6">
+            <div className="utf-footer-item-links">
+              <h3>Resources</h3>
+              <ul>
+				<li><a href="/"><i className="icon-feather-chevron-right"></i> <span>My Account</span></a></li>
+                <li><a href="/"><i className="icon-feather-chevron-right"></i> <span>Support</span></a></li>
+                <li><a href="/"><i className="icon-feather-chevron-right"></i> <span>How It Works</span></a></li>
+                <li><a href="/"><i className="icon-feather-chevron-right"></i> <span>Underwriting</span></a></li>
+                <li><a href="/"><i className="icon-feather-chevron-right"></i> <span>Employers</span></a></li>                
+              </ul>
+            </div>
+          </div>
+		  
+		  <div className="col-xl-2 col-md-3 col-sm-6">
+            <div className="utf-footer-item-links">
+              <h3>Quick Links</h3>
+              <ul>
+				<li><a href="jobs-list-layout-leftside.html"><i className="icon-feather-chevron-right"></i> <span>Jobs Listing</span></a></li>
+                <li><a href="about-us.html"><i className="icon-feather-chevron-right"></i> <span>About Us</span></a></li>
+                <li><a href="contact.html"><i className="icon-feather-chevron-right"></i> <span>Contact Us</span></a></li>
+                <li><a href="privacy-policy.html"><i className="icon-feather-chevron-right"></i> <span>Privacy Policy</span></a></li>
+                <li><a href="terms-condition.html"><i className="icon-feather-chevron-right"></i> <span>Term & Condition</span></a></li>
+              </ul>
+            </div>
+          </div>          
+        </div>
+      </div>
+    </div>
+    
+    {/* Footer Copyrights */}
+    <div className="utf-footer-copyright-item">
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-12">{`Copyright &copy; ${new Date().getFullYear()} All Rights Reserved.`}</div>
+        </div>
+      </div>
+    </div>
+     {/* Footer Copyrights / End    */}
+  </div>
+  {/* Footer / End  */}
+    </>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { CategoryBoxes } from '../components/CategoryBoxes'
+import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
 import { IntroBanner } from '../components/IntroBanner'
 import { JobLists } from '../components/JobLists'
+import { NeedHelp } from '../components/NeedHelp'
+import { Subscribe } from '../components/Subscribe'
 
 export const Home = () => {
   return (
@@ -11,6 +14,9 @@ export const Home = () => {
         <IntroBanner/>
         <CategoryBoxes/>
         <JobLists/>
+        <NeedHelp/>
+        <Subscribe/>
+        <Footer/>
     </>
   )
 }
