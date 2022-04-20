a668a78c49c5940c8b95a6473ca1c66d49972fee
Jean Francois Munyaneza <mjeanfrancois360@gmail.com>
2022-04-20 16:14:45 +0200

    Add IntroBanner component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,8 +7,8 @@ export const Home = () => {
   return (
     <>
         <Header/>
-        {/* <IntroBanner/>
-        <CategoryBoxes/> */}
+        <IntroBanner/>
+        {/* <CategoryBoxes/> */}
     </>
   )
 }
