29c92e3ce6c3a0f0572a231e8858aa98df161ee8
Jean Francois Munyaneza <mjeanfrancois360@gmail.com>
2022-05-10 10:48:04 +0200

    Add services page

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
+import { ServicesSection } from '../components/ServicesSection'
 import { Subscribe } from '../components/Subscribe'
 import { TitleBar } from '../components/TitleBar'
 
@@ -9,6 +10,7 @@ export const Services = () => {
         <>
           <Header current={'services'}/>
           <TitleBar current={'Services'} prev={'Home'} url={'/'} />
+          <ServicesSection/>
           <Subscribe/>
           <Footer/>
         </>
